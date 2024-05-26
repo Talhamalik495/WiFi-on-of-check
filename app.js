c9bc65e8a123f0bebc7f5e17afd35b1d91b3a1c8
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-05-26 20:04:20 +0500

    100% Work Done

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 let meassage_a = document.querySelector(".meassage")
+let icon_a = document.querySelector("#icon")
 const online = () => {
     meassage_a.textContent = "You are Internet Conected"
     meassage_a.style = "color:white"
+    // javascript ma style css use karna ka phal tarika 
+    icon_a.style.color = "orange"
 }
 const ofline = () => {
     meassage_a.textContent = "You are Not Internet Conected"
     meassage_a.style = "color:white"
+    // javascript ma style css use karna ka Dosra tarika 
+
+    icon_a.style = "color:white"
 
 }
 // navigator is lya use hota ha jaise browser ko detect karna ka lya 
 
 if (window.navigator.onLine) {
     online()
+
 } else {
     ofline()
 }
